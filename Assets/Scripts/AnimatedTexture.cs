35b84c1cde5288be788240985812a815d0786440
AlejandroLechon <alexlechon12@gmail.com>
2022-02-18 19:19:55 +0100

    Grandes retoques.
    Pasamos el proyecto a URP; realizamos postprocesos; ponemos materiales y ahora voy a intentar que me funcione el shader editor que no me está respondiendo. Espero que

---
+++ Assets/Scripts/AnimatedTexture.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedTexture : MonoBehaviour
+{
+    [SerializeField] Material ground;
+    [SerializeField] float speed;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //ground.SetFloat("referencedelgrapheditor", speed);
+
+    }
+}
